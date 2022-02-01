5936e2f49b575c94d73f60297b5270578b27703d
celpium <80692135+celpium@users.noreply.github.com>
2022-02-01 19:35:49 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,26 +1,96 @@
+// document.onkeydown = keyCheckOn;
+// document.onkeyup = keyCheckOff;
+// document.onmousedown = click;
+// var keyDownCtrl = 0;
+// var keyDownShift = 0;
+// function keyCheckOn(){
+//     switch(event.keyCode){
+//         case 123: event.keyCode = '';
+//         return false;
+//         break;
+//         case 17: event.keyCode = '';
+//         keyDownCtrl = 1;
+//         return false;
+//         break;
+//     }
+//     if(keyDownCtrl){
+//         return false;
+//     }
+// }
+// function keyCheckOff(){
+//     if(event.keyCode == 17){
+//         keyDownCtrl = 0;
+//     }else if(event.keyCode == 16){
+//         keyDownShift = 0;
+//     }
+// }
+// function click(){
+//     if((event.button == 2) || (event.button == 2)){
+//         alert('마우스 오른쪽 클릭, Ctrl 키, Shift 키, F12 키는 작동하지 않습니다.');
+//     }
+// }
+
+window.addEventListener("resize", contentMarginControl);
+function contentMarginControl(){
+    if(window.innerWidth > 1200){
+        $('div.marginMobile').css('display', 'none');
+        $('div.marginPC').css('display', 'block');
+    }else if(window.innerWidth <= 1200){
+        $('div.marginMobile').css('display', 'block');
+        $('div.marginPC').css('display', 'none');
+    }
+}
+$(document).ready(function(){
+    $('div.content').on('mousewheel', function(self){
+		var wheelDelta = self.originalEvent.wheelDelta;
+		if(wheelDelta > 0){
+			console.log('up');
+			$('div.content').scrollLeft(-wheelDelta + $('div.content').scrollLeft());
+		}else if(wheelDelta < 0){
+		    console.log('down');
+			$('div.content').scrollLeft(-wheelDelta + $('div.content').scrollLeft());
+        }
+	});
+    $('div.content').scroll(function(){
+        $('div.marginPC').scrollTop($('div.content').scrollTop());
+        $('div.marginPC').scrollLeft($('div.content').scrollLeft());
+    });
+    $('div.marginPC').scroll(function(){
+        $('div.content').scrollTop($('div.marginPC').scrollTop());
+        $('div.content').scrollLeft($('div.marginPC').scrollLeft());
+    });
+});
 function indexToggle(self){
     if(self.value === 'bubble'){
-        var i = 0;
-        var j = 0;
-        while(i < document.querySelectorAll('li').length){
-            document.querySelectorAll('li')[i].style.display = 'list-item';
-            i = i + 1;
-        }
-        while(j < document.querySelectorAll('li.this').length){
-            document.querySelectorAll('li.this')[j].style.borderTop = '2px solid black';
-            j = j + 1;
-        }
+        $('ul').css('display', 'block');
+        $('ul.author, ul.next-prev').css('display', 'flex');
+        $('li').css('display', 'list-item');
+        $('ul.author.this').css('border-top', '0.1rem solid black');
+        $('nav').css('height', '34.8rem');
+        $('nav').css('background-color', 'lightpink');
+        var popUrl = './image/pop.png';
+        $('input.indexToggle').css('background-image', 'url(' + popUrl + ')');
+        $('input.indexToggle').css('background-size', '1.6rem');
+        $('input.indexToggle').hover(function(){
+            $('input.indexToggle').css('filter', 'none');
+        }, function(){
+            $('input.indexToggle').css('filter', 'none');
+        });
         self.value = 'pop';
     }else if(self.value === 'pop'){
-        var i = 0;
-        var j = 0;
-        while(i < document.querySelectorAll('li:not(.this)').length){
-            document.querySelectorAll('li:not(.this)')[i].style.display = 'none';
-            i = i + 1;
-        }
-        while(j < document.querySelectorAll('li.this').length){
-            document.querySelectorAll('li.this')[j].style.borderTop = 'none';
-            j = j + 1;
+        $('ul:not(.this), li:not(.this)').css('display', 'none');
+        $('ul.author.this').css('border-top', 'none');
+        $('nav').css('height', '2.2rem');
+        $('nav').css('background-color', 'white');
+        var bubbleUrl = './image/bubble.png';
+        $('input.indexToggle').css('background-image', 'url(' + bubbleUrl + ')');
+        $('input.indexToggle').css('background-size', '1.2rem');
+        if(window.innerWidth > 1200){
+            $('input.indexToggle').hover(function(){
+                $('input.indexToggle').css('filter', 'none');
+            }, function(){
+                $('input.indexToggle').css('filter', 'grayscale(100%)');
+            });
         }
         self.value = 'bubble';
     }
