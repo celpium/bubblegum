a13ffd8d913da9cf2a0306ce8002f15726aeed48
celpium <80692135+celpium@users.noreply.github.com>
2022-02-02 11:48:22 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,34 +1,34 @@
-document.onkeydown = keyCheckOn;
-document.onkeyup = keyCheckOff;
-document.onmousedown = click;
-var keyDownCtrl = 0;
-var keyDownShift = 0;
-function keyCheckOn(){
-    switch(event.keyCode){
-        case 123: event.keyCode = '';
-        return false;
-        break;
-        case 17: event.keyCode = '';
-        keyDownCtrl = 1;
-        return false;
-        break;
-    }
-    if(keyDownCtrl){
-        return false;
-    }
-}
-function keyCheckOff(){
-    if(event.keyCode == 17){
-        keyDownCtrl = 0;
-    }else if(event.keyCode == 16){
-        keyDownShift = 0;
-    }
-}
-function click(){
-    if((event.button == 2) || (event.button == 2)){
-        alert('마우스 오른쪽 클릭, Ctrl 키, Shift 키, F12 키는 작동하지 않습니다.');
-    }
-}
+// document.onkeydown = keyCheckOn;
+// document.onkeyup = keyCheckOff;
+// document.onmousedown = click;
+// var keyDownCtrl = 0;
+// var keyDownShift = 0;
+// function keyCheckOn(){
+//     switch(event.keyCode){
+//         case 123: event.keyCode = '';
+//         return false;
+//         break;
+//         case 17: event.keyCode = '';
+//         keyDownCtrl = 1;
+//         return false;
+//         break;
+//     }
+//     if(keyDownCtrl){
+//         return false;
+//     }
+// }
+// function keyCheckOff(){
+//     if(event.keyCode == 17){
+//         keyDownCtrl = 0;
+//     }else if(event.keyCode == 16){
+//         keyDownShift = 0;
+//     }
+// }
+// function click(){
+//     if((event.button == 2) || (event.button == 2)){
+//         alert('마우스 오른쪽 클릭, Ctrl 키, Shift 키, F12 키는 작동하지 않습니다.');
+//     }
+// }
 
 $(document).ready(function(){
     $('div.content').on('mousewheel', function(self){
@@ -52,11 +52,16 @@ $(document).ready(function(){
 });
 function indexToggle(self){
     if(self.value === 'bubble'){
+        if(window.innerHeight <= 980){
+            $('nav').css('over-flow', 'scroll');
+            $('nav').css('position', 'static');
+            $('div.content').css('position', 'fixed');
+        }
         $('ul').css('display', 'block');
         $('ul.author, ul.next-prev').css('display', 'flex');
         $('li').css('display', 'list-item');
         $('ul.author.this').css('border-top', '0.1rem solid black');
-        $('nav').css('height', '34.8rem');
+        $('nav').css('height', 'auto');
         $('nav').css('background-color', 'lightpink');
         var popUrl = './image/pop.png';
         $('input.indexToggle').css('background-image', 'url(' + popUrl + ')');
@@ -66,18 +71,12 @@ function indexToggle(self){
         }, function(){
             $('input.indexToggle').css('filter', 'none');
         });
-        if(window.innerHeight <= 480){
-            $('nav').css('over-flow', 'scroll');
-            $('nav').css('position', 'static');
-            $('div.content').css('position', 'fixed');
-        }
         self.value = 'pop';
     }else if(self.value === 'pop'){
         $('ul:not(.this), li:not(.this)').css('display', 'none');
         $('ul.author.this').css('border-top', 'none');
         $('nav').css('height', '2.2rem');
         $('nav').css('background-color', 'white');
-        $('nav').css('position', 'fixed');
         var bubbleUrl = './image/bubble.png';
         $('input.indexToggle').css('background-image', 'url(' + bubbleUrl + ')');
         $('input.indexToggle').css('background-size', '1.2rem');
@@ -88,6 +87,7 @@ function indexToggle(self){
                 $('input.indexToggle').css('filter', 'grayscale(100%)');
             });
         }
+        $('nav').css('position', 'fixed');
         $('div.content').css('position', 'static');
         self.value = 'bubble';
     }
